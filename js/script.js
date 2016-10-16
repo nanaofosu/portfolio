421df4b48670074871c550077091817ee66841bb
Nana Ofosu Budu <nanaofosubudu@gmail.com>
2016-10-16 18:03:18 -0500

    added js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    //$("#hi-there").css('color','red');
+    
+    
+//    $("#hi-there").addClass("hello");
+    
+    setTimeout(function() {
+        $(".welcome").addClass("fade");
+        $("#intro").css('font-size','100px');
+    }, 2000);
+    
+    
+});
